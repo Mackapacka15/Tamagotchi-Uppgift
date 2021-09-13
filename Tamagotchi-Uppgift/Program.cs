51b88670977a51c21ff4274ea93db5d62ec28870
Markus <Markuspiotrowski@live.com>
2021-09-13 08:33:59 +0200

    Tamagotchi-Console-Klar

---
+++ Tamagotchi-Uppgift/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Tamagotchi_Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar = "";
+            bool startNew = false;
+            bool playing = true;
+            List<Tamagotchi> players = new List<Tamagotchi>();
+            players = New(players);
+
+            while (playing)
+            {
+                foreach (Tamagotchi item in players)
+                {
+
+                    if (item.GetAlive())
+                    {
+                        item.PrintStats();
+                        Console.WriteLine("What do you want to do width " + item.name + "?");
+                        Console.WriteLine("1) Teach a new word ");
+                        Console.WriteLine("2) Greet it");
+                        Console.WriteLine("3) Feed it");
+                        Console.WriteLine("4) Nothing ");
+                        Console.WriteLine("5) Start another Tamagotchi");
+                        svar = Console.ReadLine();
+
+                        if (svar == "1" || svar == "Teach a new word")
+                        {
+                            Console.WriteLine("What word do you want to teach " + item.name);
+                            string word = Console.ReadLine();
+                            item.Teach(word);
+                        }
+                        else if (svar == "2" || svar == "Greet it")
+                        {
+                            item.Hi();
+                        }
+                        else if (svar == "3" || svar == "Feed it")
+                        {
+                            item.Feed();
+                        }
+                        else if (svar == "4" || svar == "Nothing")
+                        {
+                        }
+                        else if (svar == "5" || svar == "Start another Tamagotchi")
+                        {
+                            startNew = true;
+                        }
+
+                        item.Tick();
+                    }
+                    else
+                    {
+                        Console.WriteLine(item.name + " just died");
+                        Console.WriteLine("Do you want to start another tamagotchi?");
+                        svar = Console.ReadLine();
+                        if (svar == "Yes")
+                        {
+                            startNew = true;
+                        }
+                    }
+                    Console.Clear();
+                }
+
+                if (startNew)
+                {
+                    players = New(players);
+                    startNew = false;
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+
+        static List<Tamagotchi> New(List<Tamagotchi> tamagotchis)
+        {
+            Tamagotchi tamagotchi = new Tamagotchi();
+            Console.WriteLine("What is your new tamagotchis name?");
+            tamagotchi.name = Console.ReadLine();
+            tamagotchis.Add(tamagotchi);
+            tamagotchis = Clear(tamagotchis);
+            return tamagotchis;
+        }
+        static List<Tamagotchi> Clear(List<Tamagotchi> tamagotchis)
+        {
+            List<Tamagotchi> remove = new List<Tamagotchi>();
+            foreach (Tamagotchi item in tamagotchis)
+            {
+                if (!item.GetAlive())
+                {
+                    remove.Add(item);
+                }
+            }
+            foreach (Tamagotchi item in remove)
+            {
+                tamagotchis.Remove(item);
+            }
+            return tamagotchis;
+        }
+
+    }
+}
---
+++ Tamagotchi-Uppgift/Tamagotchi.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+namespace Tamagotchi_Uppgift
+{
+    public class Tamagotchi
+    {
+        int hunger = 10;
+        int boredom = 10;
+        List<string> words = new List<string>();
+        bool isAlive = true;
+        Random generator = new Random();
+        public string name;
+
+        public void Feed()
+        {
+            hunger += generator.Next(0, 3);
+        }
+        public void Hi()
+        {
+            Console.WriteLine(words[generator.Next(0, words.Count)]);
+            ReduceBoredom();
+        }
+        public void Teach(string w)
+        {
+            words.Add(w);
+            ReduceBoredom();
+        }
+        public void Tick()
+        {
+            hunger--;
+            boredom--;
+            if (hunger <= 0 || boredom <= 0)
+            {
+                isAlive = false;
+            }
+        }
+        public void PrintStats()
+        {
+            Console.WriteLine(name + ":");
+            Console.WriteLine("Food = " + hunger);
+            Console.WriteLine("Boredom = " + boredom);
+            Console.WriteLine();
+            if (isAlive)
+            {
+                Console.WriteLine("Tamagotchi is alive");
+            }
+            else
+            {
+                Console.WriteLine("Tamagotchi is dead. Sorry");
+            }
+        }
+        public bool GetAlive()
+        {
+            return isAlive;
+        }
+        private void ReduceBoredom()
+        {
+            boredom++;
+        }
+
+
+    }
+}
